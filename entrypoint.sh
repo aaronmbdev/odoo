a0b1a9891226e0f8e8734c176acc3cd9288a3e0a
Aaron Botton <aaron@mbotton.com>
2025-08-04 21:17:32 +0200

    Fixed startup

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,18 +8,27 @@ while ! nc -z ${ODOO_DATABASE_HOST} ${ODOO_DATABASE_PORT} 2>&1; do sleep 1; done
 
 echo Database is now available
 
-exec odoo \
-    --http-port="${PORT}" \
-    --init=all \
+# Check if database exists
+DB_EXISTS=$(psql -h ${ODOO_DATABASE_HOST} -p ${ODOO_DATABASE_PORT} -U ${ODOO_DATABASE_USER} -lqt | cut -d \| -f 1 | grep -w ${ODOO_DATABASE_NAME} | wc -l)
+
+ODOO_CMD="odoo \
+    --http-port=\"${PORT}\" \
     --without-demo=True \
     --proxy-mode \
-    --db_host="${ODOO_DATABASE_HOST}" \
-    --db_port="${ODOO_DATABASE_PORT}" \
-    --db_user="${ODOO_DATABASE_USER}" \
-    --db_password="${ODOO_DATABASE_PASSWORD}" \
-    --database="${ODOO_DATABASE_NAME}" \
-    --smtp="${ODOO_SMTP_HOST}" \
-    --smtp-port="${ODOO_SMTP_PORT_NUMBER}" \
-    --smtp-user="${ODOO_SMTP_USER}" \
-    --smtp-password="${ODOO_SMTP_PASSWORD}" \
-    --email-from="${ODOO_EMAIL_FROM}" 2>&1
+    --db_host=\"${ODOO_DATABASE_HOST}\" \
+    --db_port=\"${ODOO_DATABASE_PORT}\" \
+    --db_user=\"${ODOO_DATABASE_USER}\" \
+    --db_password=\"${ODOO_DATABASE_PASSWORD}\" \
+    --database=\"${ODOO_DATABASE_NAME}\" \
+    --smtp=\"${ODOO_SMTP_HOST}\" \
+    --smtp-port=\"${ODOO_SMTP_PORT_NUMBER}\" \
+    --smtp-user=\"${ODOO_SMTP_USER}\" \
+    --smtp-password=\"${ODOO_SMTP_PASSWORD}\" \
+    --email-from=\"${ODOO_EMAIL_FROM}\""
+
+if [ "$DB_EXISTS" -eq 0 ]; then
+  echo "Initializing database with modules..."
+  ODOO_CMD="$ODOO_CMD --init=all"
+fi
+
+eval exec $ODOO_CMD 2>&1
